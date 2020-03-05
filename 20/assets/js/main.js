189329af35ae280a603504a4141ee501e442976b
Markus Tacker <Markus.Tacker@NordicSemi.no>
2020-03-05 09:12:45 +0100

    fix: lazyload carousel

--- 20/assets/js/main.js
+++ 20/assets/js/main.js
@@ -4,21 +4,6 @@ $(window).on("load", function() {
   } else {
     $("#header").removeClass("header-shrink");
   }
-  $(".carousel-inner .carousel-item").each((_, e) => {
-    const el = $(e);
-    const imgUrl = el.data("url");
-    if (imgUrl) {
-      const w = Math.floor((el.width() * 1) / 100) * 100;
-      const h = Math.floor((el.height() * 1) / 100) * 100;
-      el.css({
-        "background-image": `url('${imgUrl}?w=${w}&h=${h}&fit=crop&fm=webp&q=80')`,
-        "background-size": "cover",
-        "background-size": "cover",
-        "background-position-x": "center",
-        "background-position-y": "center"
-      });
-    }
-  });
 });
 
 $(document).ready(function() {
