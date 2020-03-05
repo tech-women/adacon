f3f8120aa659d367671a6541e93500f89b646642
Markus Tacker <m@coderbyheart.com>
2020-03-06 00:32:51 +0100

    feat: improve carousel
    Load time,
    more images

--- 20/assets/js/carousel.js
+++ 20/assets/js/carousel.js
@@ -1,15 +1,30 @@
-$(".carousel-inner .carousel-item").each((_, e) => {
-  const el = $(e);
-  const imgUrl = el.data("url");
-  if (imgUrl) {
-    const w = Math.floor((el.width() * 1) / 100) * 100;
-    const h = Math.floor((el.height() * 1) / 100) * 100;
-    el.css({
-      "background-image": `url('${imgUrl}?w=${w}&h=${h}&fit=crop&fm=webp&q=80')`,
-      "background-size": "cover",
-      "background-size": "cover",
-      "background-position-x": "center",
-      "background-position-y": "center"
-    });
+const shuffleArray = array => {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
   }
+  return array
+}
+
+$(window).on("load", () => {
+  setTimeout(() => {
+    shuffleArray($(".carousel-inner .carousel-item")).each((i, e) => {
+      const el = $(e);
+      const imgUrl = el.data("url");
+      if (imgUrl) {
+        const w = Math.floor((el.width() * 1) / 100) * 100;
+        const h = Math.floor((el.height() * 1) / 100) * 100;
+        el.css({
+          "background-image": `url('${imgUrl}?w=${w}&h=${h}&fit=crop&fm=webp&q=80')`,
+          "background-size": "cover",
+          "background-size": "cover",
+          "background-position-x": "center",
+          "background-position-y": "center"
+        });
+        if (i === 0) {
+          el.addClass('active')
+        }
+      }
+    });
+  }, 500)
 });
